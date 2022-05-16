9acd3796f90555a6a1dd57757de846c849f03892
Danielscript1 <danielsousagit@gmail.com>
2022-05-16 10:46:23 -0300

    melhorias total-carrinho

--- public/js/total-carrinho.js
+++ public/js/total-carrinho.js
@@ -1,9 +1,7 @@
 const carrinho = [39.90, 75.99, 24, 12, 109.99, 83.99]
 
 function calculoTotal(carrinho) {
-    const carrinhoTotal = carrinho.reduce((acum, valorAtual) => acum + valorAtual, 0)
-    return carrinhoTotal;
-
+    return carrinho.reduce((acum, valorAtual) => acum + valorAtual, 0)
 
 }
 //document.querySelector('#totalCarrinho').innerHTML = calculoTotal(carrinho);
