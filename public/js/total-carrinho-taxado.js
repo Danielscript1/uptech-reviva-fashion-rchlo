15303bf82823f93b041cb1213cafc76bc9097837
Danielscript1 <danielsousagit@gmail.com>
2022-05-16 11:36:24 -0300

    total-carrinho-taxado

--- public/js/total-carrinho-taxado.js
+++ public/js/total-carrinho-taxado.js
@@ -1,17 +1,28 @@
 const carrinho = [39.90, 75.99, 24, 12, 109.99, 83.99];
 
-//filtrar quais produtos a ser taxados, acima de 70
 const produtosTaxados = carrinho.filter(
-    (carrinho) => carrinho > 70)
-console.log(produtosTaxados);
+    (valoresApartir) => valoresApartir > 70)
 
-//adicionando 7% ao produtos encontrados 
-const porcentagem = produtosTaxados.map(
+const totalProdutosTaxados = produtosTaxados.map(
     taxa => (((taxa * 7) / 100) + taxa)
 
-);
-console.log(porcentagem)
+).reduce((acum, valorAtual) => acum + valorAtual, 0);
 
-//somando os valores
-const total = porcentagem.reduce((acum, valorAtual) => acum + valorAtual, 0);
-console.log(total.toFixed(2))
+
+const totalProdutosNaoTaxados = carrinho.filter(
+    (valoresNaoTaxados) => valoresNaoTaxados < 70
+).reduce((acum, valorAtual) => acum + valorAtual, 0)
+
+
+const totalCaixa = (totalProdutosTaxados, totalProdutosNaoTaxados) => {
+    return totalProdutosTaxados + totalProdutosNaoTaxados;
+}
+
+const total = totalCaixa(totalProdutosTaxados, totalProdutosNaoTaxados).toFixed(2);
+
+
+console.log("produtos sendo coletados: ", carrinho)
+console.log("produtos apartir de $70 ser taxado: ", produtosTaxados);
+console.log("total produtos taxado: ", totalProdutosTaxados)
+console.log("total produtos não taxados: ", totalProdutosNaoTaxados)
+console.log("valor da compra: ", total);
